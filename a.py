b62463a5c6a56cd2f0b62c5ffc43114b1f19278f
sakuma <sakuma@sakumanoMacBook-Pro.local>
2019-05-16 18:32:31 +0900

    update

--- a.py
+++ a.py
@@ -2,6 +2,7 @@ import argparse
 import os
 import shutil
 import time
+import json
 
 import torch
 import torch.nn as nn
@@ -107,6 +108,10 @@ if args.deterministic:
     torch.manual_seed(args.local_rank)
     torch.set_printoptions(precision=10)
 
+class Dict(dict):
+    def __getattr__(self, name): return self[name]
+    def __setattr__(self, name, value): self[name] = value
+    def __delattr__(self, name): del self[name]
 
 def main():
 
